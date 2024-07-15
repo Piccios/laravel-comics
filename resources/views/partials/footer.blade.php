1ffda032a855ca59a29214f3097a9c7df7842686
Piccio <lorenzo.hl.picchi@gmail.com>
2024-07-15 19:33:36 +0200

    socials link footer's part completed

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,33 +5,60 @@
                 <ul class="footerPartOne">
                     <h2> dc comics </h2>
                     @foreach ($FooterPartOne as $link)
-                    <li>
-                        <a href="">{{$link['name']}}</a>
-                    </li>
+                        <li>
+                            <a href="">{{ $link['name'] }}</a>
+                        </li>
                     @endforeach
                 </ul>
                 <ul class="footerShop">
                     <h2>shop</h2>
                     @foreach ($FooterShop as $link)
-                    <li>
-                        <a href="">{{$link['name']}}</a>
+                        <li>
+                            <a href="">{{ $link['name'] }}</a>
                     @endforeach
-                    </ul>
+                </ul>
                 <ul class="footerPartTwo">
                     <h2>dc</h2>
                     @foreach ($FooterPartTwo as $link)
-                    <li>
-                        <a href="">{{$link['name']}}</a>
-                    </li>
+                        <li>
+                            <a href="">{{ $link['name'] }}</a>
+                        </li>
                     @endforeach
                 </ul>
                 <ul class="footerPartThree">
                     <h2>sites</h2>
                     @foreach ($FooterPartThree as $link)
-                    <li>
-                        <a href="">{{$link['name']}}</a>
-                    </li>
+                        <li>
+                            <a href="">{{ $link['name'] }}</a>
+                        </li>
                     @endforeach
+                </ul>
+            </div>
+        </section>
+    </div>
+    <div class="socials">
+        <section class="containerSocials">
+            <div class="button">
+                <span class="button-footer">sign-up now!</span>
+            </div>
+            <div class="social">
+                <h3>fallow us</h3>
+                <ul>
+                    <li>
+                        <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="Facebook">
+                    </li>
+                    <li>
+                        <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="Twitter">
+                    </li>
+                    <li>
+                        <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="youTube">
+                    </li>
+                    <li>
+                        <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="Pinterest">
+                    </li>
+                    <li>
+                        <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="Periscope">
+                    </li>
                 </ul>
             </div>
         </section>
