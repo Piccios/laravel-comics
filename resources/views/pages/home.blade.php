f8f936a844e15f80faffdd1577b37d375eff4218
Piccio <lorenzo.hl.picchi@gmail.com>
2024-07-15 18:14:42 +0200

    main content data imported via for each loop

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,12 +1,29 @@
 @extends('layout.app')
 @section('page-title', 'Homepage')
 
+@section('header')
+    <section class="container-header">
 
+    </section>
 
 @section('jumbo-tron')
-        <section class="container-jumbo">
-            <div><p>current series</p></div>
-        </section>
+    <section class="container-jumbo">
+        <div><p>current series</p></div>
+    </section>
+@endsection
+
+@section('main-content')
+    <section class="list-products">
+        @foreach ($comics as $comic)
+    <article class="series_comics">
+            <img src="{{ $comic['thumb'] }}" alt="{{$comic['title']}}">
+            <p>
+                {{$comic['title']}}
+            </p>
+        </article>
+        @endforeach
+        <div><p>Load more</p></div>
+    </section>
 @endsection
 
 
