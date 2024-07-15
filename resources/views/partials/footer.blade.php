c88db808b2948f85fab46f75db159856194c210d
Piccio <lorenzo.hl.picchi@gmail.com>
2024-07-15 19:07:55 +0200

    footer layout started with "dc comics" and "shop" links set for test

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,23 @@
 <footer>
-    
+    <div>
+        <secrtion class="container-links">
+            <div>
+                <ul class="footerPartOne">
+                    <h2> dc comics </h2>
+                    @foreach ($FooterPartOne as $link)
+                    <li>
+                        <a href="">{{$link['name']}}</a>
+                    </li>
+                    @endforeach
+                </ul>
+                <ul class="FooterShop">
+                    <h2>shop</h2>
+                    @foreach ($FooterShop as $link)
+                    <li>
+                        <a href="">{{$link['name']}}</a>
+                    @endforeach
+                    </ul>
+            </div>
+        </secrtion>
+    </div>
 </footer>
