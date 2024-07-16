392fe673a82b3f94432036916d6a4a5a3bca8bcb
Piccio <lorenzo.hl.picchi@gmail.com>
2024-07-16 12:37:48 +0200

    main page is empty now and does not show the products

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,23 +4,9 @@
 
 @section('jumbo-tron')
     <section class="container-jumbo">
-        <div><p>current series</p></div>
     </section>
 @endsection
 
-@section('main-content')
-    <section class="list-products">
-        @foreach ($comics as $comic)
-    <article class="books">
-            <img src="{{ $comic['thumb'] }}" alt="{{$comic['title']}}">
-            <p>
-                {{$comic['title']}}
-            </p>
-        </article>
-        @endforeach
-        <div><p>Load more</p></div>
-    </section>
-@endsection
 
 @section('info')
 
